f8f93e43ef7995276af9fb5be4e087efed01ed6f
ZingerMachine <95539586+ZingerMachine@users.noreply.github.com>
2024-08-05 19:38:38 +0300

    Add files via upload

---
+++ Sample/MenuTasks.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sample
+{
+    class MenuTasks
+    {
+        public static void Invoke()
+        {
+            bool repeat = true;
+            do
+            {
+                Console.WriteLine("\nEnter number list tasks:");
+
+                string? num = Console.ReadLine();
+
+                Tasks task = new Tasks();
+
+                switch (num)
+                {
+                    case "1":
+                        {
+                            task.GetNames();
+                            break;
+                        }
+                    case "2":
+                        {
+                            task.GetTruth();
+                            break;
+                        }
+                    case "0":
+                        {
+                            repeat = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine($"No list tasks");
+                            break;
+                        }
+                }
+            } while (repeat);
+        }
+    }
+}
---
+++ Sample/Tasks.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample
+{
+    internal class Tasks
+    {
+        public void GetNames()
+        {
+            string[] names = ["Kate", "Tom", "Sam", "Mike", "Alice"];
+
+            foreach (string name in names)
+            {
+                Console.WriteLine($"{name} has {name.Length} characters.");
+            }
+        }
+
+        public void GetTruth()
+        {
+            bool a = true;
+            bool b = false;
+
+            Console.WriteLine($"AND | a | b ");
+            Console.WriteLine($"a | {a & a,-5} | {a & b,-5} ");
+            Console.WriteLine($"b | {b & a,-5} | {b & b,-5} ");
+            Console.WriteLine();
+            Console.WriteLine($"OR | a | b ");
+            Console.WriteLine($"a | {a | a,-5} | {a | b,-5} ");
+            Console.WriteLine($"b | {b | a,-5} | {b | b,-5} ");
+            Console.WriteLine();
+            Console.WriteLine($"XOR | a | b ");
+            Console.WriteLine($"a | {a ^ a,-5} | {a ^ b,-5} ");
+            Console.WriteLine($"b | {b ^ a,-5} | {b ^ b,-5} ");
+        }
+    }
+}
