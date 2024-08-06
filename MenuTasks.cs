8cff3e3ceba785f87144359c9854f62b7af8e524
ZingerMachine <95539586+ZingerMachine@users.noreply.github.com>
2024-08-06 22:57:55 +0300

    Add files via upload

---
+++ MenuTasks.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sample
+{
+    class MenuTasks
+    {
+        public static void Invoke()
+        {
+            bool repeat = true;
+            do
+            {
+                Console.WriteLine("\nEnter number list tasks:");
+
+                string? num = Console.ReadLine();
+
+                Tasks task = new Tasks();
+
+                switch (num)
+                {
+                    case "1":
+                        {
+                            task.GetNames();
+                            break;
+                        }
+                    case "2":
+                        {
+                            task.GetTruth();
+                            break;
+                        }
+                    case "3":
+                        {
+                            task.Task_03();
+                            break;
+                        }
+                    case "4":
+                        {
+                            task.Task_04();
+                            break;
+                        }
+                    case "5":
+                        {
+                            task.Task_05();
+                            break;
+                        }
+                    case "0":
+                        {
+                            repeat = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine($"No list tasks");
+                            break;
+                        }
+                }
+            } while (repeat);
+        }
+    }
+}
---
+++ Tasks.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample
+{
+    internal class Tasks
+    {
+        public void GetNames()
+        {
+            string[] names = ["Kate", "Tom", "Sam", "Mike", "Alice"];
+
+            foreach (string name in names)
+            {
+                Console.WriteLine($"{name} has {name.Length} characters.");
+            }
+        }
+
+        public void GetTruth()
+        {
+            bool a = true;
+            bool b = false;
+
+            Console.WriteLine($"AND | a | b ");
+            Console.WriteLine($"a | {a & a,-5} | {a & b,-5} ");
+            Console.WriteLine($"b | {b & a,-5} | {b & b,-5} ");
+            Console.WriteLine();
+            Console.WriteLine($"OR | a | b ");
+            Console.WriteLine($"a | {a | a,-5} | {a | b,-5} ");
+            Console.WriteLine($"b | {b | a,-5} | {b | b,-5} ");
+            Console.WriteLine();
+            Console.WriteLine($"XOR | a | b ");
+            Console.WriteLine($"a | {a ^ a,-5} | {a ^ b,-5} ");
+            Console.WriteLine($"b | {b ^ a,-5} | {b ^ b,-5} ");
+        }
+
+        public void Task_03()
+        {
+            for (; true;);
+        }
+
+        public void Task_04()
+        {
+            for (int i = 1; i < 15; i++)
+            {
+                Console.WriteLine($"{i}! = :N0");
+            }
+        }
+
+        public void Task_05()
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(
+            File.CreateText(Path.Combine(Environment.GetFolderPath(
+            Environment.SpecialFolder.DesktopDirectory), "log.txt"))));
+
+            Trace.AutoFlush = true;
+
+            Debug.WriteLine("Debug says, I am watching!");
+            Trace.WriteLine("Trace says, I am watching!");
+        }
+    }
+}
